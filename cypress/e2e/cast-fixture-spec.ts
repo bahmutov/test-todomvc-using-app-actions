b8cfff256fc52c45b4500b64baad6013f46a7f2c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-29 10:35:58 -0400

    cast fixture yielded value

---
+++ cypress/e2e/cast-fixture-spec.ts
@@ -0,0 +1,20 @@
+import type { Todo } from './model'
+
+describe('Use JSON fixture', () => {
+  it('adds todos following the fixture', () => {
+    cy.visit('/')
+    cy.fixture<{ todos: Todo[] }>('todos.json')
+      .its('todos')
+      .should('be.an', 'array')
+      .then((todos) => {
+        todos.forEach((todo, k) => {
+          cy.get('input.new-todo').type(todo.title + '{enter}')
+          cy.get('.todo-list li').should('have.length', k + 1)
+          if (todo.completed) {
+            cy.get('.todo-list li').last().find('.toggle').click()
+            cy.get('.todo-list li').last().should('have.class', 'completed')
+          }
+        })
+      })
+  })
+})
