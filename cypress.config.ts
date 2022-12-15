3cad34527aab03b22d6ba745c7891cd8f3123e6c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-15 07:58:25 -0500

    use run all specs button

--- cypress.config.ts
+++ cypress.config.ts
@@ -18,5 +18,6 @@ export default defineConfig({
     baseUrl: 'http://localhost:8888',
     excludeSpecPattern: ['*.page.ts', 'utils.ts', '*.d.ts'],
     specPattern: 'cypress/e2e/**/*spec.{js,ts}',
+    experimentalRunAllSpecs: true,
   },
 })
